cfd1cc03a39bdbb91d396e7513129dc7df2c2ad5
Matin Imam <84035118+matin676@users.noreply.github.com>
2024-05-31 15:17:05 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+import os
+
+from art import logo,vs
+from game_data import data
+
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+def format_data(account):
+    """Takes the account data and returns the printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+def check_answer(guess, a_follower_count, b_follower_count):
+    """Take the user guess and follower counts and returns if they got it right."""
+    if a_follower_count > b_follower_count:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+while game_should_continue:
+    account_a = account_b
+    account_b = random.choice(data)
+
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}")
+
+    guess = input("Who has more followers? 'A' or 'B': ").lower()
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    os.system("cls")
+    print(logo)
+
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}.")
